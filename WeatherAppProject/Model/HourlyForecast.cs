b65a7bb15ab5f35b66aee33e66c18e4739259bdc
DersimCloudHT24 <dersim.abbas@iths.se>
2024-12-16 15:29:24 +0100

    dropdowns for daily forecasts still in progress.

---
+++ WeatherAppProject/Model/HourlyForecast.cs
@@ -0,0 +1,9 @@
+﻿namespace WeatherAppProject.Model
+{
+    public class HourlyForecast
+    {
+        public string Time { get; set; } // Time of the forecast (e.g., 12:00 PM)
+        public int Temp { get; set; } // Temperature in int
+        public string Condition { get; set; } // Weather description
+    }
+}
